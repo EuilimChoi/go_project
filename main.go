7b0d59e57b3a34b5cf7c08717915df61f874d47f
EuilimChoi <91925895+EuilimChoi@users.noreply.github.com>
2022-06-23 18:22:24 +0900

    크롤링 로직 구현중

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+}
+
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
@@ -22,6 +29,36 @@ func main() {
 func getPage(page int) {
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Req ", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".cardOutline")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		// id, _ := card.Attr("class")
+		// splitID := strings.Split(id, " ")[4][4:]
+		// title := card.Find(".jcs-JobTitle").Text()
+		// location := card.Find(".companyLocation").Text()
+		// summery := card.Find(".job-snippet").Text()
+		salary := card.Find(".attribute_snippet").Text()
+		fmt.Println(salary)
+	})
+}
+
+func extractJob(card *goquery.Selection) {
+	// id, _ := card.Attr("class")
+	// splitID := strings.Split(id, " ")[4][4:]
+	// title := card.Find(".jcs-JobTitle").Text()
+	// location := card.Find(".companyLocation").Text()
+	salary := card.Find(".attribute_snippet").Text()
+	// summery := card.Find(".job-snippet").Text()
+	fmt.Println(salary)
 }
 
 func getPages() int {
@@ -38,7 +75,6 @@ func getPages() int {
 	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
 		pages = s.Find("a").Length()
 	})
-
 	return pages
 }
 
